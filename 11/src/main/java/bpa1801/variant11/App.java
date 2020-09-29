df5cd8c42b8b8fe83970fdc6769e6efa32e6cc5e
Artem Shinkaruk <artasend@gmail.com>
2020-09-29 12:41:12 +0700

    Create hello world application

---
+++ 11/src/main/java/bpa1801/variant11/App.java
@@ -0,0 +1,13 @@
+package bpa1801.variant11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
