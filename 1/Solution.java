f179aef4b005b43d4c30cd918cf5815aa80b3cfc
Alina Strusevich <strusevich.a.d@gmail.com>
2020-10-13 14:12:30 +0700

    Solution is added

--- 1/Solution.java
+++ 1/Solution.java
@@ -1,6 +1,35 @@
-public class FirstJavaProgram {
-   
-   public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+import java.util.*;
+
+public class Solution {
+	/*
+	*  Дано n - число уровней. Построить треугольник из символов #.
+    * Пример для n = 4:
+	*	   #	
+	*	  ##
+	*	 ###
+	*	####
+    */	
+
+	    
+	static void func1(int n) {
+    	for(int stroke = 1; stroke <= n; stroke++){
+			for (int symbol = 1; symbol <= n - stroke; symbol++){
+				System.out.print(' ');
+			}
+			for (int symbol = 1; symbol <= stroke; symbol++){
+				System.out.print('#');
+			}
+			System.out.println(' ');
+		}
+       }
+    
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		
+		func1(n);
+		}
+		  
+	}
