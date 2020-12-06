7fc0ab891ebbd4e315aa0e4eeba8095702517861
Artem Shinkaruk <artasend@gmail.com>
2020-12-07 01:02:56 +0700

    Finalize editor app

--- 11/src/main/java/bpa1801/variant11/EditorApp.java
+++ 11/src/main/java/bpa1801/variant11/EditorApp.java
@@ -1,6 +1,8 @@
 package bpa1801.variant11;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import bpa1801.variant11.editor.Command;
@@ -17,13 +19,25 @@ public class EditorApp
 
     private static final State state = new State();
 
+    private static final ArrayList<Command> commands = new ArrayList<Command>();
+
     public static void main(String[] args) throws IOException {
         System.out.println("Specify scene file");
         readScene(scanner.nextLine());
         System.out.println("Specify commands file");
-        applyCommands(scanner.nextLine());
-        for (Figure figure: state.buffer.getFigures()) {
-            System.out.println(figure.serialize());
+        readCommands(scanner.nextLine());
+        run();
+        System.out.println("Specify output file");
+        writeScene(scanner.nextLine());
+    }
+
+    public static void run() {
+        for (Command command : commands) {
+            if (command.getName() != "undo") {
+                state.commands.push(command);
+            }
+            System.out.println(String.format("Applying comand: %s", command.toString()));
+            command.apply(state.buffer);
         }
     }
 
@@ -42,21 +56,28 @@ public class EditorApp
         System.out.println(String.format("Loaded scene with %d figure(s)", state.buffer.size()));
     }
 
-    public static void applyCommands(String file) {
+    public static void readCommands(String file) {
         FileLineReader reader = new FileLineReader(file);
         String line = reader.next();
         while (line != null) {
             Command command = CommandFactory.deserialize(line, state);
             if (command != null) {
-                if (command.getName() != "undo") {
-                    state.commands.push(command);
-                }
-                System.out.println(String.format("Applying comand: %s", command.toString()));
-                command.apply(state.buffer);
+                commands.add(command);
             } else {
                 System.out.println(String.format("Wrong command line: %s", line));
             }
             line = reader.next();
+        }
+        System.out.println(String.format("Loaded %d command(s)", commands.size()));
+    }
+
+    public static void writeScene(String file) {
+        try (FileWriter writer = new FileWriter(file)) {
+            for (Figure figure: state.buffer.getFigures()) {
+                writer.append(String.format("%s%s", figure.serialize(), System.lineSeparator()));
+            }
+        } catch (IOException ex) {
+            System.out.println(String.format("Failed to write to file, reason: %s", ex.getMessage()));
         }
     }
 }
