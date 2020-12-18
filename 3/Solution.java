0719f64aeaa9907c9ac0f72cdb9eaa3eeceab4a1
Anastasia44 <71637557+Anastasia44-ch@users.noreply.github.com>
2020-12-18 13:25:47 +0700

    Версия с разделением на потоки

--- 3/Solution.java
+++ 3/Solution.java
@@ -1,34 +1,42 @@
-import java.io.*;
 import java.util.*;
 
-public class Solution {
-
-    static int func(int[] ar) {
-    int max = ar[0];
-    int k = 0;
-    for (int i = 0; i < ar.length; i++) {
-        if (ar[i]>max){
-            max=ar[i];
+class Candles extends Thread
+{
+    @Override
+    public void run()
+    {
+        int max = Solution.ar[0];
+        int k = 0;
+        for (int i = 0; i < Solution.ar.length; i++) {
+            if (Solution.ar[i] > max) {
+                max = Solution.ar[i];
+            }
         }
-    }
-    for (int i = 0; i < ar.length; i++) {
-        if (ar[i] == max){
-            k++;
+        for (int i = 0; i <  Solution.ar.length; i++) {
+            if ( Solution.ar[i] == max) {
+                k++;
+            }
         }
+        System.out.println("Количество самых высоких свечей: " + k);
+        System.out.println("Вывод результатов дочернего тока закончен");
     }
-    return k;
 }
 
+public class Solution {
+    public static int[] scores;
     private static final Scanner scanner = new Scanner(System.in);
+    static int[] ar;
+    
+    public static void main(String[] args){
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("OUTPUT_PATH"));
+        Candles Cn = new Candles(); // Создание потока
+        System.out.println("Запуск главного потока");
 
         System.out.println("Введите количество свечей: ");
         int arCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int[] ar = new int[arCount];
+        ar = new int[arCount];
 
         System.out.println("Введите высоту свечей: ");
         String[] arItems = scanner.nextLine().split(" ");
@@ -38,12 +46,9 @@ public class Solution {
             int arItem = Integer.parseInt(arItems[i]);
             ar[i] = arItem;
         }
-        int result = func(ar);
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
 
-        bufferedWriter.close();
-
+        Cn.start();
+        System.out.println("Главный поток закончен");
         scanner.close();
     }
 }
