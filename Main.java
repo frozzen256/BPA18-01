fc0a32d20619d41d78d83797b66142d45d4a376c
SultanovAziz <71460117+SultanovAziz@users.noreply.github.com>
2020-12-29 08:21:44 +0300

    Потоки.Изменение ошибок

--- Main.java
+++ Main.java
@@ -6,11 +6,56 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main (String [] args){
+        //Создание потока
+        Theard theard =new Theard( );
+        //Ввод данных
+        String[] nkq = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nkq[0]);
+
+        int k = Integer.parseInt(nkq[1]);
+
+        int q = Integer.parseInt(nkq[2]);
+
+        int[] a = new int[n];
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+        //Добавление вводиых данных
+        theard.GetValues(a,k,queries);
+        //Запуск и вывод вычислений
+        theard.start();
 
 
+    }
+}
+class Theard extends Thread implements IOperations {
+    private  int [] a;
+    private  int k;
+    private int [] queries;
 
-    static int[] ArrayRot(int[] a, int k, int[] queries) {
+    public void GetValues(int[] a, int k, int[] queries){
+        this.a=a;
+        this.k=k;
+        this.queries=queries;
+    }
+
+    public void run (){
         int [] array=new int[a.length];
         for (int i=0;i<k;i++){
             int temp=a[a.length-1];
@@ -22,55 +67,20 @@ public class Main {
         try {
             for (int i=0;i<a.length;i++){
                 array[i]=a[queries[i]];
+                System.out.print(array[i]);
             }
-            return array;
+
         }
         catch(Exception e){
             System.out.printf("Error index of array queries is not equal to array a ");
-            return new int [a.length];
+
         }
 
     }
 
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        new Thread( () -> {
-            String[] nkq = scanner.nextLine().split(" ");
-
-            int n = Integer.parseInt(nkq[0]);
-
-            int k = Integer.parseInt(nkq[1]);
-
-            int q = Integer.parseInt(nkq[2]);
-
-            int[] a = new int[n];
-
-            String[] aItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int i = 0; i < n; i++) {
-                int aItem = Integer.parseInt(aItems[i]);
-                a[i] = aItem;
-            }
-
-            int[] queries = new int[q];
-
-            for (int i = 0; i < q; i++) {
-                int queriesItem = scanner.nextInt();
-                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-                queries[i] = queriesItem;
-            }
-
-            int[] result = ArrayRot(a, k, queries);
-            scanner.close();
-
-        }
-                ).start();
-
-
-    }
+}
+@FunctionalInterface
+interface IOperations{
+    void GetValues (int[] a, int k, int[] queries);
 }
